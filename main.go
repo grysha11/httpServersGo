b9b6ce23411184716a0a8f86b3b4e62a104aced4
grysha11 <grzhzahar@gmail.com>
2026-01-12 18:46:03 +0100

    add middleware, metrics, default handlers

--- main.go
+++ main.go
@@ -1,15 +1,59 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
+	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
+	bodyText := "OK"
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(bodyText))
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	numOfReq := "Hits: " + strconv.FormatInt(int64(cfg.fileserverHits.Load()), 10)
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(numOfReq))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	bodyText := "RESET"
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(bodyText))
+}
 
 func main() {
 	mux := http.NewServeMux()
-	server := http.Server{}
-	server.Addr = ":8080"
-	server.Handler = mux
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: mux,
+	}
+	cfg := &apiConfig{}
+	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("/healthz", handleHealthz)
+	mux.HandleFunc("/metrics", cfg.handleMetrics)
+	mux.HandleFunc("/reset", cfg.handleReset)
 	err := server.ListenAndServe()
 	fmt.Printf("Error during listen and serve: %v\n", err)
 }
